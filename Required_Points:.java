60a4d93577123c66f73811d7c0c4d265bf2769de
SabbellaLaharika <119842850+SabbellaLaharika@users.noreply.github.com>
2024-01-06 11:52:57 +0530

    import java.util.*; public class RequiredPoints {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int b = sc.nextInt();         int points;         points = b/a;         System.out.println(points);     } }

---
+++ Required_Points:.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class RequiredPoints
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int points;
+        points = b/a; 
+        System.out.println(points);
+    }
+}

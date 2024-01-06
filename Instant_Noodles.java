34ff248aaabbf31b3c8ca323cf3c3f7ca5251224
SabbellaLaharika <119842850+SabbellaLaharika@users.noreply.github.com>
2024-01-06 11:55:36 +0530

    import java.util.*; public class ServeInstantNoodles {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int b = sc.nextInt();         int maximumCustomers;         maximumCustomers = b*a;         System.out.println(maximumCustomers);     } }

---
+++ Instant_Noodles.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ServeInstantNoodles
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int maximumCustomers;
+        maximumCustomers = b*a; 
+        System.out.println(maximumCustomers);
+    }
+}

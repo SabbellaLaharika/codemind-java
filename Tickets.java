709a50e44ca8b471b06886fffcfe3ffe2b04beea
SabbellaLaharika <119842850+SabbellaLaharika@users.noreply.github.com>
2024-01-11 10:55:05 +0530

    import java.util.*; public class Concert {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int x = sc.nextInt();         System.out.print(4*x <= 1000 ? "YES" : "NO");     } }

---
+++ Tickets.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Concert
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.print(4*x <= 1000 ? "YES" : "NO");
+    }
+}

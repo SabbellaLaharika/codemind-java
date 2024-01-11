61fdb762e94cb36d0ecb882adf5fe346e9ffd0e4
SabbellaLaharika <119842850+SabbellaLaharika@users.noreply.github.com>
2024-01-11 10:00:29 +0530

    import java.util.*; public class Conversion {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         double res = (a*5)/18.0;         System.out.printf("%.2f",res);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Conversion
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        double res = (a*5)/18.0;
+        System.out.printf("%.2f",res);
+    }
+}
